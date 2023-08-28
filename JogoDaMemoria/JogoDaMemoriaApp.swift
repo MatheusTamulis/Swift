ec64e09c86810fea05c3349821654e2403027e48
Matheus <matheustamulis@gmail.com>
2023-08-27 22:51:30 -0300

    Initial Commit

---
+++ JogoDaMemoria/JogoDaMemoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogoDaMemoriaApp.swift
+//  JogoDaMemoria
+//
+//  Created by Ana Paula Barbosa Zacatei on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct JogoDaMemoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
